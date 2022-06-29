9c19127f059c24078bed616114279415081b5915
irscy <47720070+irscy@users.noreply.github.com>
2022-06-29 07:34:22 -0500

    Created sprite move animations
    Plays when player changes directions or starts moving

--- src/script/move_controller.gd
+++ src/script/move_controller.gd
@@ -24,15 +24,24 @@ var can_play_down_anim : bool
 @export var idle_spr : String
 
 func _ready() -> void:
-	set_object_values()
+	set_variable_values()
+	fix_transform()
 	set_indexed("position:y", get_viewport_rect().size.y * 0.5 + 75)
 	
 func _physics_process(delta) -> void:
 	enable_move_controls(delta)
 	
-# MOVEMENT
+func set_variable_values() -> void:
+	defined_sprite = get_node("Sprite")
+	flip_anim = get_node("Sprite/FlipAnim")
+	
+func fix_transform() -> void:
+	defined_sprite.scale = Vector2(0.6, 0.35)
+	
+# move controls (obviously)
 func enable_move_controls(delta : float) -> void:
-	# if left key is held down
+	# moving #
+	# if left key is held down (make sure moving vertically and right is false)
 	if Input.is_action_pressed("Move Left") && !right && !vertic:
 		global_position -= x_speed * delta
 		scale = Vector2(1, 1)
@@ -49,7 +58,7 @@ func enable_move_controls(delta : float) -> void:
 		defined_sprite.playing = false
 		
 		
-	# if right key is held down	
+	# if right key is held down (make sure moving vertically and left is false)
 	if Input.is_action_pressed("Move Right") && !left && !vertic:
 		global_position += x_speed * delta
 		scale = Vector2(-1, 1)
@@ -64,8 +73,34 @@ func enable_move_controls(delta : float) -> void:
 		horiz = false
 		defined_sprite.animation = idle_spr
 		defined_sprite.playing = false
-
-
-func set_object_values() -> void:
-	defined_sprite = get_node("Sprite")
-	flip_anim = get_node("Sprite/FlipAnim")
+		
+	
+	# if up key is held down (make sure moving down is false)
+	if Input.is_action_pressed("Move Up") && !down && !horiz:
+		global_position -= y_speed * delta
+		up = true
+		vertic = true
+		defined_sprite.animation = up_spr
+		defined_sprite.playing = true
+		
+	# if up key is released
+	if Input.is_action_just_released("Move Up"):
+		up = false
+		vertic = false
+		defined_sprite.animation = idle_spr
+		defined_sprite.playing = false
+		
+		
+	# if down key is held down (make sure moving up is false)
+	if Input.is_action_pressed("Move Down") && !up && !horiz:
+		global_position += y_speed * delta
+		down = true
+		vertic = true
+		defined_sprite.animation = down_spr
+		defined_sprite.playing =  true
+		
+	if Input.is_action_just_released("Move Down"):
+		down = false
+		vertic = false
+		defined_sprite.animation = idle_spr
+		defined_sprite.playing = false
